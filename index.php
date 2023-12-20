c7ea7591861c6bb3184b7c7e331daba301870bb7
Davide Gamberini <davide.jk.gamberini@gmail.com>
2023-12-20 15:54:16 +0100

    Creazione delle cartelle e dei primi file utili Clona la repository direttamente in c://xampp/htdocs e tienilo all'interno della directory in cui lo genera Con le run configuration che ho messo non dovremmo avere problemi

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+include './include/funzioni.inc';
+
+$titolo = 'Home Page BF Drinks';
+$css = './styles/myStyle.css';
+stampa_head($titolo, $css);
+
+echo "<h1>$titolo</h1>";
+
+
+
