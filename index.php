07ac1e7a90c552f2e6bb66b95ebc49d57b814638
Davide Gamberini <davide.jk.gamberini@gmail.com>
2023-12-22 12:06:48 +0100

    Aggiunta formattazione per i dati

--- index.php
+++ index.php
@@ -11,7 +11,17 @@ $titolo = 'Home Page BF Drinks';
 $css = './styles/myStyle.css';
 stampa_head($titolo, $css);
 
-echo "<h1>$titolo</h1>";
+echo "<h1>BF Drinks</h1>";
+echo '<footer>
+    <div id="banner">
+        <div class="scrolling-text">
+            Dal Belluzzi per il Belluzzi, le bibite energetiche che ti fanno stare attento ad ogni lezione!
+        </div>
+    </div>
+</footer>';
+
+stampa_finehtml();
+
 
 
 
