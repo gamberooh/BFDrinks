ebced6a6ec67837a9b4fb86ddd790c727b5e8994
Davide Gamberini <davide.jk.gamberini@gmail.com>
2023-12-22 19:08:35 +0100

    Aggiunta di controllo.js0, cercaProdotti e aggiunti alcuni campi form

--- index.php
+++ index.php
@@ -10,16 +10,50 @@ include './include/funzioni.inc';
 $titolo = 'Home Page BF Drinks';
 $css = './styles/myStyle.css';
 stampa_head($titolo, $css);
+?>
 
-echo "<h1>BF Drinks</h1>";
-echo '<footer>
+<header>
+    <!<!-- menu a tendina -->
+</header>
+
+<div class="container">
+    <div class="header">
+        <h1>BF Drinks</h1>
+    </div>
+    <div class="item">
+        <form id="form1" method="post" action="cercaProdotti.php">
+            <span>Nome bibita:</span><input type="text" name="nome" size="50">
+            
+            <!-- 
+            array('nome' => '', 'linea' => '', 'gassata' => '', 'preferita' => '', 'calorie' => 0, 'sede' => '','img-prodotto' => ''),
+            -->
+            
+            <span>Linea</span>
+            
+            <span>Liscia o gassata:</span>
+            
+            <span>Preferita della classe:</span>
+            
+            <span>Calorie</span><input type="number">
+            
+            <span>Sede</span>
+            
+            
+            
+        </form>
+    </div>
+</div>
+
+
+<footer>
     <div id="banner">
         <div class="scrolling-text">
-            Dal Belluzzi per il Belluzzi, le bibite energetiche che ti fanno stare attento ad ogni lezione!
+            Dal Belluzzi per il Belluzzi, BF Drinks ti fa stare attento ad ogni lezione!
         </div>
     </div>
-</footer>';
+</footer>
 
+<?php
 stampa_finehtml();
 
 
