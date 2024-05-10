a4f6d860c028faeda1658641ff6805aa263c1200
Davide Gamberini <davide.jk.gamberini@gmail.com>
2024-05-11 00:39:48 +0200

    Creazione signup e gestione accessi Gestione accessi: collegamento al db Da fixare alcuni errori

--- login.php
+++ login.php
@@ -5,19 +5,33 @@
     $css = './styles/myStyle.css';
     stampa_head($titolo, $css);
     echo "<h1 class='header'>Entra in BF Drinks</h1>";
+    
+    // LOG IN
+    
     echo '<div class="container">';
     echo "<form method='post' action='index.php'>"
             . "<div class='element'>"
-                . "<span>Username: </span><input type='text' name='username'>"
+                . "<span>Username: </span><input type='text' name='Username'>"
             . "</div>"
             . "<div class='element'>"
-                . "<span>Password: </span><input type='password' name='password'>"
+                . "<span>Password: </span><input type='password' name='Pswd'>"
             . "</div>"
             . "<div class='button'>"
                 . "<input type='submit' name='invio' value='Login'>"
             . "</div>"
-        . "</form>";
+        . "</form>"; 
     echo '</div>';
+    
+    // SIGN UP
+    
+    echo "<div class='container'>"
+        . "<form method='post' action='signup.php'>"
+            . "<div class='button'>"
+                . "<input type='submit' name='invio' value='Signup'>"
+            . "</div>"
+        . "</form>"
+    . "</div>";
+    
     stampa_finehtml();
     
     
---
+++ signup.php
@@ -0,0 +1,52 @@
+<?php
+
+
+    include './include/funzioni.inc';    
+    $titolo = 'Pagina di login del sito PHP';
+    $css = './styles/myStyle.css';
+    stampa_head($titolo, $css);
+    echo "<h1 class='header'>Entra in BF Drinks</h1>";
+
+// Username Pswd Email Nome Cognome Telefono Classe (Ruolo)
+
+echo '<div class="container">';
+    echo "<form method='post' action='index.php'>"
+            . "<div class='item'>"
+                . "<div class='element'>"
+                    . "<span>Name: </span><input type='text' name='Nome'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Surname: </span><input type='text' name='Cognome'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Username: </span><input type='text' name='Username'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Password: </span><input type='password' name='Pswd'>"
+                . "</div>"
+                
+            . "</div"
+            . "<div class='item'>"
+                . "<div class='element'>"
+                    . "<span>Email: </span><input type='mail' name='Email'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Telefono: </span><input type='number' name='Telefono'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Classe: </span><input type='mail' name='Classe'>"
+                . "</div>"
+                . "<div class='element'>"
+                    . "<span>Profile picture: </span><input type = \"file\" name='propic'/>" //immagine profilo
+                . "</div>"
+                . "<input type='hidden' name='Ruolo' value='user'>"
+                     
+                
+                . "<div class='element'>"
+                    . "<div class='button'>"
+                        . "<input type='submit' name='invio' value='Access'>"
+                    . "</div>"
+                . "</div>"
+            . "</div>"
+        . "</form>"; 
+    echo '</div>';
