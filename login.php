d12f422fd637b8aa5ca548d8d22c9b226838142e
Davide Demelas <davidedemelas.nba@gmail.com>
2024-05-12 02:14:19 +0200

    Miglioramento CSS pagina di log-in

--- login.php
+++ login.php
@@ -1,40 +1,43 @@
 <?php
 
-        include './include/funzioni.inc';    
-    $titolo = 'Pagina di login del sito PHP';
-    $css = './styles/myStyle.css';
-    stampa_head($titolo, $css);
-    echo "<h1 class='header'>Entra in BF Drinks</h1>";
-    
-    // LOG IN
-    
-    echo '<div class="container">';
-    echo "<form method='post' action='index.php'>"
-            . "<div class='element'>"
-                . "<span>Username: </span><input type='text' name='Username'>"
-            . "</div>"
-            . "<div class='element'>"
-                . "<span>Password: </span><input type='password' name='Pswd'>"
-            . "</div>"
-            . "<div class='button'>"
-                . "<input type='submit' name='invio' value='Login'>"
-            . "</div>"
-        . "</form>"; 
-    echo '</div>';
-    
-    // SIGN UP
-    
-    echo "<div class='container'>"
-        . "<form method='post' action='signup.php'>"
-            . "<div class='button'>"
-                . "<input type='submit' name='invio' value='Signup'>"
-            . "</div>"
-        . "</form>"
-    . "</div>";
-    
-    stampa_finehtml();
-    
-    
+include './include/funzioni.inc';
+$titolo = 'Pagina di login del sito PHP';
+$css = './styles/myStyle.css';
+$classe_body = 'login-page';
+stampa_head($titolo, $css, $classe_body);
 
 
+// LOG IN
+
+echo '<div class="container">';
+echo "<h1 class='header'>Entra in BF Drinks</h1>";
+echo "  <form method='post' action='index.php'>"
+   . "      <div class='element'>"
+   . "          <input type='text' name='Username' placeholder='Username...'>"
+   . "      </div>"
+   . "      <div class='element'>"
+   . "          <input type='password' name='Pswd' placeholder='Password...'>"
+   . "      </div>"
+   . "      <div class='button'>"
+   . "          <input type='submit' name='invio' value='Login'>"
+   . "      </div>"
+   . "  </form>";
+echo "<div class=\"sign-up-link\">  "
+.       "<a href=\"signup.php\"> Non sei ancora registrato? Fallo ora.</a>"
+.    "</div>";
+echo '</div>';
+
+// SIGN UP
+/*
+echo "<div class='container'>"
+ . "    <form method='post' action='signup.php'>"
+ . "  <div class='button'>"
+ . "    <input type='submit' name='invio' value='Signup'>"
+ . "  </div>"
+ . "</form>"
+ . "</div>";*/
+
+
+
+stampa_finehtml();
 
