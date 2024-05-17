460f77cea299f4a254e747ebff2d6a2923c00534
SABRINA INFANTINO <niccoautocad@gmail.com>
2024-05-17 18:56:05 +0200

    Update della pagina user-profile.php

--- user-details/user-profile.php
+++ user-details/user-profile.php
@@ -2,19 +2,43 @@
     
     session_start();
     include './include/funzioni.inc';    
-    $titolo = 'Pagina di login del sito PHP';
+    $titolo = 'User details';
     $css = './styles/myStyle.css';
+
     stampa_head($titolo, $css);
-    if ($_SESSION['logged_in']) {
+
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method == 'POST')
+        $input = $_POST;
+    else
+        $input = $_GET;
+
+
+    if (isset($_SESSION['logged']) and $_SESSION['logged']) {
         echo "<h1 class='header'>$_SESSION[Nome] $_SESSION[Cognome] profile page</h1>";
         echo "<div>"
-                . "<img sr"
+                . "<img src=\"./images/img-profile/".$_SESSION["nome"].$_SESSION["cognome"]."\">"
             . "</div>";
-    
+
+        $sql = "SELECT U.Email, U.Telefono, U.Classe FROM Utente U WHERE U.Username = :Username";
+        $bind['Username']['val'] = $_SESSION['username'];
+        $bind['Username']['tipo'] = PDO::PARAM_STR;
+
+        $result = esegui_query_con_bind($sql, $bind);
+        echo "<table>";
+        foreach($result as $colonne){ 
+            echo "<tr>";
+            foreach($colonne as $chiave => $valore){ 
+                echo "<td>$chiave: $valore</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        /*echo "<div><p>User's Email: ".$_SESSION["Email"]."</p></div>";
+        echo "<div><p>User's Telefono: ".$_SESSION["Telefono"]."</p></div>";
+        echo "<div><p>User's Classe: ".$_SESSION["Classe"]."</p></div>";*/
     }
-    
-    
-    
     
     torna_home_page();
     
