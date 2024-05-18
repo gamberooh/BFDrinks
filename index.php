146ef6c79f8ddb05c8f7b5fcd2caa0a42c239bf7
Davide Demelas <davidedemelas.nba@gmail.com>
2024-05-18 09:52:04 +0200

    miglioramenti CSS navbar e index, aggiunta annunci e foto logo

--- index.php
+++ index.php
@@ -5,12 +5,20 @@ include './include/funzioni.inc';
 include './include/connection.php';
 
 $titolo = 'Home Page BF Drinks';
-$css = './styles/myStyle.css;';
+$css = './styles/myStyle.css';
+$classebody = "index-page";
 $script = './scripts/controlloErrori.js';
 
-stampa_head($titolo, $css);
+stampa_head($titolo, $css, $classebody);
 include_script($script);
 
 check_login(isAdmin());
+
+ad_banner("images/img-annunci/annuncio_zenzerozero.png");
+ad_banner("images/img-annunci/annuncio_powerboom.png");
+ad_banner("images/img-annunci/annuncio_zuccherofilato.png");
+ad_banner("images/img-annunci/annuncio_tropical.png");
+?>
+<?php
 stampa_finehtml();
 
